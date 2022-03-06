297c04f45c12e9a4b116de37de9531a83a6be836
Georgi <cerber1982@abv.bg>
2022-03-06 15:39:19 +0200

    a new integer created sout multiply

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         int numberA = 10;
+        int numberB = 5;
 
-        System.out.println(numberA);
+        System.out.println(numberA * numberB);
     }
 }
